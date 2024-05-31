6781b32a3424080da3602076a8e7be226efe74e4
Deeksha R K <deeksha24rk@gmail.com>
2024-05-31 13:58:37 +0530

    Created Footer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,6 +1,11 @@
 import "./footer.scss";
 const Footer = () => {
-  return <div className="footer">Footer</div>;
+  return (
+    <div className="footer">
+      <span>Dashboard</span>
+      <span> &copy; Admin Dashboard</span>
+    </div>
+  );
 };
 
 export default Footer;
