3092660c2c573cc0d2760eba8ae452deffd4f22c
Deeksha R K <deeksha24rk@gmail.com>
2024-05-31 13:09:18 +0530

    Created Navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,34 @@
 import "./navbar.scss";
+
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  return (
+    <div className="navbar">
+      {/* navbar - left */}
+      <div className="logo">
+        <img src="logo.svg" alt="logo image" />
+        <span>Dashboard</span>
+      </div>
+
+      {/* navbar - right */}
+      <div className="icons">
+        <img src="/search.svg" alt="" className="icon" />
+        <img src="/app.svg" alt="" className="icon" />
+        <img src="/expand.svg" alt="" className="icon" />
+        <div className="notification">
+          <img src="/notifications.svg" alt="" />
+          <span>1</span>
+        </div>
+        <div className="user">
+          <img
+            src="https://images.unsplash.com/photo-1544005313-94ddf0286df2?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fHByb2ZpbGUlMjBwaG90b3xlbnwwfHwwfHx8MA%3D%3D"
+            alt=""
+          />
+          <span>Jane</span>
+        </div>
+        <img src="/settings.svg" alt="" className="icon" />
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
