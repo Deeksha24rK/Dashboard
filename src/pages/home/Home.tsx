4c44deb3ad59ea761e8d4913ffef0e35e464f5dd
Deeksha R K <deeksha24rk@gmail.com>
2024-06-02 18:32:08 +0530

    line charts

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,12 @@
-import TopBox from "../../components/topBox/topBox";
+import TopBox from "../../components/topBox/TopBox";
+import ChartBox from "../../components/chartBox/chartBox";
 import "./home.scss";
+import {
+  chartBoxConversion,
+  chartBoxProduct,
+  chartBoxRevenue,
+  chartBoxUser,
+} from "../../data";
 
 const Home = () => {
   return (
@@ -8,11 +15,19 @@ const Home = () => {
         <div className="box box1">
           <TopBox />
         </div>
-        <div className="box box2">Box2</div>
-        <div className="box box3">Box3</div>
+        <div className="box box2">
+          <ChartBox {...chartBoxUser} />
+        </div>
+        <div className="box box3">
+          <ChartBox {...chartBoxProduct} />
+        </div>
         <div className="box box4">Box4</div>
-        <div className="box box5">Box5</div>
-        <div className="box box6">Box6</div>
+        <div className="box box5">
+          <ChartBox {...chartBoxRevenue} />
+        </div>
+        <div className="box box6">
+          <ChartBox {...chartBoxConversion} />
+        </div>
         <div className="box box7">Box7</div>
         <div className="box box8">Box8</div>
         <div className="box box9">Box9</div>
