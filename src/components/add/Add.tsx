c49931c953710703482bba32ffd640397849b98f
Deeksha R K <deeksha24rk@gmail.com>
2024-06-04 11:43:47 +0530

    Added Users and Products page

---
+++ src/components/add/Add.tsx
@@ -0,0 +1,42 @@
+import { GridColDef } from "@mui/x-data-grid";
+import "./add.scss";
+
+type Props = {
+  slug: string;
+  columns: GridColDef[];
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+const Add = (props: Props) => {
+  const { slug, columns, setOpen } = props;
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault(); //Doesnt refresh the page after clicking the button
+
+    //add new item
+    // axios.post(`/api/${slug}s` , {})
+  };
+
+  return (
+    <div className="add">
+      <div className="modal">
+        <span className="close" onClick={() => setOpen(false)}>
+          X
+        </span>
+        <h1>Add new {slug}</h1>
+        <form onSubmit={handleSubmit}>
+          {columns
+            .filter((item) => item.field !== "id" && item.field !== "img")
+            .map((column) => (
+              <div className="item">
+                <label>{column.headerName}</label>
+                <input type={column.type} placeholder={column.field} />
+              </div>
+            ))}
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Add;
